f017c42cfeaf9b81e83f04a2fc73df98c960e028
Dmitry S <dsagal+git@gmail.com>
2017-08-01 01:36:39 -0400

    Add index.js a build step to build a debug and minified bundle (11K at the moment)

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+exports.computed = require('./lib/computed.js');
+exports.dispose = require('./lib/dispose.js');
+exports.dom = require('./lib/dom.js');
+exports.domevent = require('./lib/domevent.js');
+exports.emit = require('./lib/emit.js');
+exports.observable = require('./lib/observable.js');
+exports.subscribe = require('./lib/subscribe.js');
+exports.util = require('./lib/util.js');
+
+exports.Disposable = exports.dispose.Disposable;
+exports.Emitter = exports.emit.Emitter;
+exports.bundleChanges = exports.observable.bundleChanges;

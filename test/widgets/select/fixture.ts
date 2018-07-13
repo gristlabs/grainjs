a472967ccc0fa0192f1294821d38600461470aee
Dmitry S <dsagal+git@gmail.com>
2018-07-13 12:00:37 -0400

    Some refactoring of select tests

--- test/widgets/select/fixture.ts
+++ test/widgets/select/fixture.ts
@@ -1,34 +1,48 @@
-import {IOption, select} from 'lib/widgets/select/select';
+import {IOption, IOptionFull, select} from 'lib/widgets/select/select';
 import {dom, obsArray, observable} from '../../../index';
-
-function createOption(val: string) {
-  const colon = val.indexOf(":");
-  if (colon === -1) { return val; }
-  return {
-    value: val.slice(0, colon),
-    label: val.slice(colon + 1),
-    disabled: val.startsWith("-"),
-  };
-}
 
 function testDom() {
   const value = observable("apple");
   const choices = obsArray<IOption<string>>(["acorn", "apple", "orange", "papaya"]);
 
   let textarea: HTMLTextAreaElement;
-  return dom('div#test_select',
-    select(value, choices, {defLabel: "Select a fruit:"}),
-    dom('div', "Current value: ",
-      dom('input#test_value', {type: 'text'}, dom.prop('value', value),
-        dom.on('input', (e, elem) => value.set((elem as HTMLInputElement).value))),
-    ),
-    textarea = dom('textarea', {rows: "10"}, choices.get().join("\n")) as HTMLTextAreaElement,
-    dom('div',
-      dom('input', {type: 'button', value: 'Update'},
-        dom.on('click', () => choices.set(textarea.value.trim().split("\n").map(createOption))),
+
+  function parseChoices() {
+    choices.set(textarea.value.trim().split("\n"));
+  }
+
+  const richChoices: Array<IOptionFull<object>> = [
+    {value: {a: 1}, label: "A1"},
+    {value: {b: 2}, label: "B2", disabled: true},
+    {value: {c: 3}, label: "C3"},
+  ];
+  const richValue = observable(richChoices[2].value);
+
+  return [
+    dom('div#test_main',
+      select(value, choices, {defLabel: "Select a fruit:"}),
+      dom('div', "Current value: ",
+        dom('input#test_value', {type: 'text'}, dom.prop('value', value),
+          dom.on('input', (e, elem) => value.set((elem as HTMLInputElement).value))),
+      ),
+      textarea = dom('textarea', {rows: "10"}, choices.get().join("\n"),
+        dom.on('blur', parseChoices),
+      ) as HTMLTextAreaElement,
+      dom('div',
+        dom('input', {type: 'button', value: 'Update'}, dom.on('click', parseChoices)),
       ),
     ),
-  );
+    dom('div#test_array',
+      select(value, ["apple", "cherry", "tomato", "orange"]),
+      dom('div', "Current value: ", dom('br'),
+        dom('b', dom.text(value))),
+    ),
+    dom('div#test_rich',
+      select(richValue, richChoices),
+      dom('div#test_rich_value', "Current value:", dom('br'),
+        dom('b', dom.text((use) => JSON.stringify(use(richValue))))),
+    ),
+  ];
 }
 
-dom.update(dom.find('#root')!, testDom());
+dom.update(document.body, testDom());
--- test/widgets/select/testSelect.ts
+++ test/widgets/select/testSelect.ts
@@ -1,6 +1,9 @@
 import {server} from '../test-server';
 import {assert, driver, useServer} from '../webdriver-mocha';
 
+// TODO READ HELPFUL RECOMMENDATIONS AT:
+// https://wiki.saucelabs.com/display/DOCS/Best+Practices+for+Running+Tests
+
 describe('select', () => {
   useServer(server);
 
@@ -9,13 +12,29 @@ describe('select', () => {
     await driver.get(`${server.getHost()}/select/fixture.html`);
   });
 
-  it('should find element', async () => {
-    // TODO READ RECOMMENDATIONS AT
-    // https://wiki.saucelabs.com/display/DOCS/Best+Practices+for+Running+Tests
-    assert.equal(await driver.find('#test_select select').getAttribute('value'), 'apple');
+  it('should update observable when value is selected', async () => {
+    assert.equal(await driver.find('#test_main select').getAttribute('value'), 'apple');
     assert.equal(await driver.find('#test_value').getAttribute('value'), 'apple');
-    await driver.findContent('#test_select option', /papaya/).click();
-    assert.equal(await driver.find('#test_select select').getAttribute('value'), 'papaya');
+    await driver.findContent('#test_main option', /papaya/).click();
+    assert.equal(await driver.find('#test_main select').getAttribute('value'), 'papaya');
     assert.equal(await driver.find('#test_value').getAttribute('value'), 'papaya');
+  });
+
+  it.only('should update UI when observable changes', async () => {
+    await driver.find('#test_value').doClear().sendKeys('foo');
+    assert.equal(await driver.find('#test_main select').getAttribute('value'), 'Select a fruit:');
+    assert.equal(await driver.find('#test_array select').getAttribute('value'), '');
+    await driver.find('#test_value').sendKeys('orange');
+    assert.equal(await driver.find('#test_value').getAttribute('value'), 'orange');
+    assert.equal(await driver.find('#test_array select').getAttribute('value'), 'orange');
+    await driver.find('#test_value').sendKeys('cherry');
+    assert.equal(await driver.find('#test_value').getAttribute('value'), 'Select a fruit:');
+    assert.equal(await driver.find('#test_array select').getAttribute('value'), 'cherry');
+  });
+
+  it('should support non-string values', async () => {
+    assert.equal(await driver.find('#test_rich_value').getText(), '{"a": 1}');
+    await driver.findContent('#test_rich_values options', /^C/).click();
+    assert.equal(await driver.find('#test_rich_value').getText(), '{"c": 0}');
   });
 });

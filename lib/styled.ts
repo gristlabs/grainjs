3aa0c94464f75edf2a8f629506075742adc74a4d
Dmitry S <dsagal+git@gmail.com>
2018-10-29 16:28:20 -0400

    Fix styled module when there is no global window object

--- lib/styled.ts
+++ lib/styled.ts
@@ -125,11 +125,14 @@ function createCssRules(className: string, styles: string) {
   return `& {${mainRules}\n}\n${nestedRules}`.replace(/&/g, className);
 }
 
+// Used by getNextStyleNum when running without a global window object (e.g. in tests).
+const _global = {};
+
 // Keep the counter for next class attached to the global window object rather than be a library
 // global. This way if by some chance multiple instance of grainjs are loaded into the page, it
 // still works without overwriting class names (which would be extremely confusing).
 function getNextStyleNum() {
-  const g = (G.window as any);
+  const g: any = G.window || _global;
   return g._grainNextStyleNum = (g._grainNextStyleNum || 0) + 1;
 }
 

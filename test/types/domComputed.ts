301b0e9b6b8a4a82b61af2a55e7d9fb83227c861
Dmitry S <dsagal+git@gmail.com>
2019-04-17 10:15:43 -0400

    Add a typings test for the dom.maybe() change

--- test/types/domComputed.ts
+++ test/types/domComputed.ts
@@ -9,3 +9,31 @@ dom.domComputed((use) => 1, (val: number) => '' + val);   // $ExpectType [Node, 
 dom.domComputed((use) => 1);                              // $ExpectError
 dom.domComputed((use) => use(obs).toUpperCase());         // $ExpectType [Node, Node, DomMethod]
 dom.domComputed((use) => dom('div'));                     // $ExpectType [Node, Node, DomMethod]
+
+// Check that dom.maybe passes non-null types to its callback.
+interface Foo {
+  str: string;
+  nstr: string|null;
+  ustr?: string|Element;
+  foo?: Foo;
+  fstr: string|Foo|null|undefined;
+}
+const foo: Foo = null as any;
+dom.maybe(foo.str, (val) =>
+  val);       // $ExpectType string
+dom.maybe(foo.nstr, (val) =>
+  val);       // $ExpectType string
+dom.maybe(foo.ustr, (val) =>
+  val);       // $ExpectType string | Element
+dom.maybe(foo.foo, (val) => {
+  val;        // $ExpectType Foo
+  return null;
+});
+dom.maybe(foo.fstr, (val) => {
+  val;        // $ExpectType string | Foo
+  return null;
+});
+dom.domComputed(foo.nstr, (val) =>              // Unlike maybe(), domComputed() passes type as is
+  val);       // $ExpectType string | null
+dom.domComputed(foo.ustr, (val) =>              // Unlike maybe(), domComputed() passes type as is
+  val);       // $ExpectType string | Element | undefined
